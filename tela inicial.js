2970e9c8f9406f30eb28a1bd7fdd11d7820989ab
Samuel Montessi do Amaral Silva <samuel.montessi@estudante.ufjf.br>
2025-08-11 21:51:16 -0300

    adiciona evento botão de créditos

--- tela inicial.js
+++ tela inicial.js
@@ -2,3 +2,7 @@ const comecar = document.getElementById("Start");
 const creditos = document.getElementById("Creditos");
 const mensagem = document.getElementById("MensagemCreditos");
 const fechar = document.getElementById("Fechar");
+
+creditos.addEventListener("click", () => {
+    mensagem.style.display = "block";
+});
