52276a4f06499f8261d5c109a544f49f2dd2d7f9
Samuel Montessi do Amaral Silva <samuel.montessi@estudante.ufjf.br>
2025-08-11 21:49:40 -0300

    identifica variáveis

---
+++ tela inicial.js
@@ -0,0 +1,4 @@
+const comecar = document.getElementById("Start");
+const creditos = document.getElementById("Creditos");
+const mensagem = document.getElementById("MensagemCreditos");
+const fechar = document.getElementById("Fechar");
